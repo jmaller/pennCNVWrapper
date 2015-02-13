e4bdae84140fbce3e88f3a90003e2ea8c725cdbe
tpoterba <tpoterba@broadinstitute.org>
2015-02-13 14:10:53 -0500

    Signal file prep done

--- pennCNVWrapper.py
+++ pennCNVWrapper.py
@@ -9,26 +9,49 @@
         -n NAME, --name=NAME                Names the run of pennCNVWrapper
 
 """
-
+import os
+import subprocess
 
 
 from docopt import docopt
 
 
+def prepSignalFiles(dataFolder, outputFolder, force=False):
+    # CONVERT ILLUMINA FILES TO PENNCNV INPUT SIGNAL FILES
+    gtcList = []
+    try:
+        for root, dirs, files in os.walk(dataFolder):
+            for file in files:
+                if file.endswith(".gtc.txt"):
+                    gtcList.append(os.path.join(root, file))
+    except OSError:
+        fatal("file structure exception")
 
-def prepSignalFiles():
-    ## CONVERT ILLUMINA FILES TO PENNCNV INPUT SIGNAL FILES
-    # echo "converting illumina files to penncnv format"
-    # for i in `cat $gtclist`
+    # Check if the signalfiles folder exists, or create if it doesn't
+    signalPath = outputFolder + "/signalfiles"
+    if not os.path.isdir(signalPath):
+        os.mkdir(signalPath)
+    logPath = signalPath + "/processLog.txt"
+    logFile = open(logPath, 'w')
+
+    # convert each gtc file
+    for gtcFile in gtcList:
+        # Grab the sample ID from the file path
+        sampleID = gtcFile.split("/")[-1].split(".")[0]
+        samplePath = signalPath + "/" + sampleID + ".gtc.txt.penncnv"
+        if not (os.path.isfile(samplePath) and force is False):
+            toWrite = open(samplePath, 'w')
+            toWrite.write("%s\t%s\t%s\t%s\t%s\n" % ("Name", "Chr", "Position", sampleID+".Log R Ratio", sampleID+".B Allele Freq"))
+            os.system("awk -v OFS='\t' 'NR>12 {print $1, $2, $3, $16, $15}' " + gtcFile + " + >>" + samplePath)
+
+
     # do
     #    bname=`basename $i .gtc.txt`
     #    outfile=${base}/data/signalfiles/${i}.penncnv
     #    rm $outfile
     #    printf "%s\t%s\t%s\t%s\t%s\t%s\n" "Name" "Chr" "Position" "${bname}.GType"  "${bname}.Log R Ratio"  "${bname}.B Allele Freq" > $outfile
     #    cat ${base}/data/wave1/$i | awk -v OFS='\t' 'NR>12 {print $1, $2, $3, $7 $8, $16, $15}' >>$outfile
-
     # done
-
     #find $dpath/signalfiles/ -iname '*.g
 
 def prepPFBFile():
@@ -54,7 +77,9 @@ def postRunSummary():
     pass
 
 
-
+def fatal(errorMessage):
+    print "ERROR: " + errorMessage
+    exit()
 
 
 
